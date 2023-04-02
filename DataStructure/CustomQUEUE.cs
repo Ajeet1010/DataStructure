90a2fccc549cc8933ffaaff9824ded9f35af36e0
Ajeet <ajeetpal101999@gmail.com>
2023-04-02 21:45:09 +0530

    [Ajeet] Insertion of element using Generics in QUEUE.

---
+++ DataStructure/CustomQUEUE.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class CustomQUEUE<T>
+    {
+        public Node<T> head;
+        public void Enqueue(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            if (head == null)
+            {
+                head = newNode;
+                Console.WriteLine("{0} is inserted into the Queue ", head.data);
+            }
+            else
+            {
+                Node<T> temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+                Console.WriteLine("{0} is inserted into the Queue ", newNode.data);
+            }
+        }
+        
+        // displaying of node.
+        public void Display()
+        {
+            Console.WriteLine("QUEUE");
+            if (head == null)
+            {
+                Console.WriteLine("Queue is Empty");
+            }
+            else
+            {
+                Node<T> temp = head;
+                Console.WriteLine("Elements in QUEUE are:  ");
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -5,10 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("*****  Data Structure problems  *****");
-            CustomStack<int> list = new CustomStack<int>();
-            list.Add(48);
-            list.Add(40);
-            list.Add(102);
+            CustomQUEUE<double> list = new CustomQUEUE<double>();
+            list.Enqueue(48.12);
+            list.Enqueue(40);
+            list.Enqueue(10.2);
             list.Display();
         }
     }
