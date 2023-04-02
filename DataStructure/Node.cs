ab1852023cab3545a192f38f7b96a01808625221
Ajeet <ajeetpal101999@gmail.com>
2023-04-02 21:37:54 +0530

    [Ajeet] Insertion of element using Generics in STACK.

--- DataStructure/Node.cs
+++ DataStructure/Node.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataStructure
 {
-    // UC1- Using custom generic linked list.
+    // UC2- Using custom generic STACK.
     internal class Node<T>
     {
         public T data;
--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("*****  Data Structure problems  *****");
-            CustomLinkedList<int> list = new CustomLinkedList<int>();
+            CustomStack<int> list = new CustomStack<int>();
             list.Add(48);
             list.Add(40);
             list.Add(102);
